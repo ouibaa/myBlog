eca5a671de3802b8c9f44091ec9692f431291012
Weber <wliu1996@hotmail.com.au>
2020-01-05 20:20:48 +1100

    Updates

---
+++ component---src-pages-about-js-d0c4f758aefd3dc2bd40.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{"3XHS":function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),r=n.n(a),c=n("LbRr");t.default=function(){return r.a.createElement("div",{style:{color:"teal"}},r.a.createElement(c.a,{headerText:"About GatsbyJS"}),r.a.createElement("p",null,"Such wow. Very React."))}},LbRr:function(e,t,n){"use strict";var a=n("q1tI"),r=n.n(a);t.a=function(e){return r.a.createElement("h1",null,e.headerText)}}}]);
+//# sourceMappingURL=component---src-pages-about-js-d0c4f758aefd3dc2bd40.js.map
---
+++ component---src-pages-index-js-b3e1e289f305a8e11f89.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{RXBc:function(e,t,a){"use strict";a.r(t);var n=a("q1tI"),c=a.n(n),l=a("Wbzz");t.default=function(){return c.a.createElement("div",{style:{color:"purple"}},c.a.createElement(l.Link,{to:"/contact/"},"Contact"),c.a.createElement("h1",null,"Hello Gatsby!"),c.a.createElement("p",null,"What a world."),c.a.createElement("img",{src:"https://source.unsplash.com/random/400x200",alt:""}))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-b3e1e289f305a8e11f89.js.map
